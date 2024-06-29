6d4bfaadeedb2a4cdb674e2bd4709fc27500a14a
Zauri Barbaqadze <125299632+ZauriBarbaqadze@users.noreply.github.com>
2024-06-29 05:59:07 +0400

    Project Init

---
+++ src/pages/card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import man from "@/assets/Man.jpg"
+
+const index = () => {
+  return (
+    <div className='wrapper'>
+      <div className='image'><img src={man} alt="" /></div>
+    </div>
+  )
+}
+
+export default index
